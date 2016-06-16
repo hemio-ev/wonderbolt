3bb6cdede33a3e74aeca82fa7e91d239ec2fcc83
Michael Herold <quabla@hemio.de>
2016-06-16 13:50:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,230 @@
+Wonderbold
+==========
+
+Wonderbold is a config based email modifier, acting as local delivery agent and
+resubmitting emails via SMTP after modification.
+
+Wonderbold's main purpose is to enable simple mailinglists with external
+management of subscribers.
+
+-   Depends: python >= 3.4
+-   Recommends: python3-systemd
+
+Commandline Options
+-------------------
+
+### `--config`
+
+Required. Can be given multiple times. Later config files overwrite
+values from earlier ones.
+
+### `--sasl-username`
+
+Optional. See config optione `require_sasl_username`.
+
+Configuration Options
+---------------------
+
+### `msg_bounced_requirements`
+
+*string*
+
+Message contained in bounce email delivered if `require_` configurations
+are violated.
+
+### `header_add`
+
+*mapping*
+
+Adds header values without removing/changing existing header lines.
+Performed after `header_replace`. Therefore, `header_add` adds a header
+a second time if key overlaps with `header_replace` are present.
+
+### `header_replace`
+
+*mapping*
+
+Removes all existing headers with the given keys and adds the new
+values.
+
+### `envelope_mail_from`
+
+*string* or *`null`*
+
+Bounce address for the new delivery of the email. If this setting is
+`null`, it will be derived from the `From` email header.
+
+### `envelope_rcpt_to`
+
+*list of strings* or *`null`*
+
+Recipients for new delivery of the email. If this setting is `null`, it
+will be derived from the `To` email header.
+
+### `require_from`
+
+*list of strings* or *`"envelope_rcpt_to"`* or *`false`*
+
+Enables to reject mails if the `From:` email header does not fulfill a
+condition.
+
+-   *list of strings*: `From` must be contained in this list.
+-   *`"envelope_rcpt_to"`*: `From` must be contained in the
+    `envelope_rcpt_to` list. If this option is set, `envelope_rcpt_to`
+    must be set and not `null`.
+-   *`false`*: Disables checks (default).
+
+### `require_sasl_username`
+
+*list of strings* or *`"envelope_rcpt_to"`* or *`false`*
+
+Enables to reject mails if the SASL username does not fulfill a
+condition. Same parameters as for `require_from`.
+
+For this feature to work, `--sasl-username $(sasl_username)` has to be
+passed.
+
+### `sasl_recipient_delimiter`
+
+*string*
+
+Recipient delimiters only affect the interpretation of `envelope_rcpt_to` if
+`require_sasl_username` is set to this value. The localpart of all recipients
+is stripped from all characters occuring after a first delimiter for comparing
+with the sasl username.
+
+This option should be set to the same value as the *recipient_delimiter* option
+of the postfix server. Every character in the string is treated as a delimiter.
+An empty string (default) corresponds to no delimiters.
+
+### `smtp_server`
+
+*string*
+
+SMTP server via which the email is submitted. Default is "localhost:25".
+
+Example Configuration
+---------------------
+
+```yaml
+{
+    "msg_bounced_requirements": "Bounced due to unprivileged SASL user",
+    "header_add": {
+        "X-Header-1": 1,
+        "X-Header-3": "Value 3"
+    },
+    "header_replace": {
+        "X-Header-1": "Value 1",
+        "X-Header-2": "Value 2"
+    },
+    "envelope_mail_from": "Bounce To <bounce_to@example.com>",
+    "envelope_rcpt_to": [
+        "User Name <user@example.org>",
+        "user2@example.org"
+    ],
+    "require_from": false,
+    "require_sasl_username": "envelope_rcpt_to",
+    "smtp_server": "mail.example.com:25"
+}
+```
+
+Complete Mailinglist under Postfix
+----------------------------------
+
+*master.cfg*
+
+ ```
+wonderbolt   unix  -       n       n       -       -       pipe
+  flags=Ohu
+  user=ldaml
+  argv=/usr/local/bin/wonderbolt.py
+   --config /etc/wonderbolt/${recipient}.json
+   --sasl-username $(sasl_username)
+```
+
+*main.cfg*
+
+    wonderbolt_destination_recipient_limit = 1
+
+*/etc/wonderbolt/list@example.org.json*
+
+```json
+{
+    "envelope_mail_from": "list_bounce@example.org",
+    "envelope_rcpt_to": [
+        "listadmin@example.org",
+        "user1@example.com",
+        "user2_lists@example.com"
+    ],
+    "header_replace": {
+        "List-ID": "<test.example.org>",
+        "List-Help": "<mailto:listadmin@example.org>",
+        "List-Post": "<mailto:list@example.org>",
+        "List-Unsubscribe": "<mailto:listadmin@example.org?body=unsubscribe%20list%20list@example.org>",
+        "List-Subscribe:": "<mailto:listadmin@example.org?body=subscribe%20list%20list@example.org>",
+        "Precedence": "list"
+    },
+    "require_sasl_username": "envelope_rcpt_to",
+    "sasl_recipient_delimiter": "_"
+}
+```
+
+*maps/aliases* (postfix `virtual_alias_maps`)
+
+    list_bounce@example.org listadmin@example.org
+
+*maps/trasport* (postfix `transport_maps`)
+
+    kist@example.org wonderbolt:
+
+
+Dedication
+----------
+
+
+                                           ooooooo                
+                                       oO0.....00OOOoo            
+                                    oO0...........0Oo             
+                                  oO000000000000000000o           
+                            o00Ooo000000000000000000OoOOo         
+                            0....000000000000000000000o           
+                            ...........0000000000000000o          
+                            0...............000..00000000o        
+                     oOoO0OO..................O..0Oo0oooooo       
+                     O........0.....0OOooo..O0....O 0O            
+                  00OO0.......000....00o  ooO.::.......o          
+                  O............000.....00o  O.:.......0           
+                   O...........000000......0......00oo            
+                    o0.........000000000...........0o             
+                      o0........0000000.......0OOoo               
+                    OOOO0.......0O0000000......0o       oooo      
+                    O............0OO000000.......O    o0....0O    
+                     oO...........0OO000000.......00OO........0o  
+                        oO.........0OO00.00..............00....0o 
+                           o0.......OOO0..0............0oo......0 
+                           O........0OO.................o 0......o
+                            O0.......00.................0  O0...0 
+              ooooooooo        oo0.......................o   ooo  
+          oOOOOOOOOOOO000OOOoo  0::......................         
+        OOOOOOOOO000000000000000...0.............Oo0....O         
+       OOOOOOO000000...........................Oo   oOOo          
+      oO oOOO00000.::..00OOoooO00...........0o                    
+         0OO00000::.000Oo    0.............0o                     
+         0OO0000.:.0000      o............0o                      
+         OOO0000.:.0000o      o..........0                        
+          OOO0000.:.00OOo      O..........o                       
+           OOO0000::.00ooo     o..........0                       
+            oOO0000.:.00O      o...........O                      
+              OO0000.:.000o     oO0.........O                     
+               oO00000.:.OOO        0.......0o                    
+                 oO0000.:.ooOo      oO00....0o                    
+                   oO000.:.o oO                                   
+                     oOO0.:0   o                                  
+                       o O.0                                      
+                          Oo                                      
+
+
+GitLab Thumbnail Copyright
+[BlackGryph0n](http://blackgryph0n.deviantart.com/art/Rainbow-Dash-204973879)
+Licensed under [CC BY-NC 3.0](https://creativecommons.org/licenses/by-nc/3.0/)
+
---
+++ wonderbolt.py
@@ -0,0 +1,321 @@
+#!/usr/bin/env python3
+
+#  Wonderbolt
+#  Copyright (C) 2016 Michael Herold <quabla@hemio.de>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# PROG_NAME
+PROG_NAME = 'wonderbolt'
+
+# LOG_LEVEL
+# Possible values: ERROR, WARNING, INFO, DEBUG
+LOG_LEVEL = 'DEBUG'
+
+try:
+    from systemd.journal import JournalHandler
+    LOG_HANDLER = JournalHandler(SYSLOG_IDENTIFIER=PROG_NAME)
+except ImportError:
+    from logging.handlers import SysLogHandler
+    LOG_HANDLER = SysLogHandler('/dev/log')
+    LOG_HANDLER.ident = PROG_NAME + ': '
+
+import logging
+
+LOG = logging.getLogger(__name__)
+LOG.addHandler(LOG_HANDLER)
+LOG.setLevel(LOG_LEVEL)
+LOG.debug("Logger initialized: %s", type(LOG_HANDLER))
+
+# get rid of any output immediatley after logging is in place
+import io
+import sys
+
+POSTFIX_PIPE_ERR = sys.stderr
+sys.stdout = io.StringIO()
+sys.stderr = io.StringIO()
+
+try:
+    import argparse
+    import collections
+    import email
+    import email.utils
+    import json
+    import smtplib
+
+    class AuthenticationError(Exception):
+        pass
+
+    def err(msg):
+        global LOG
+        LOG.error('Error on executing %s:\n%s', sys.argv, msg)
+        raise Exception()
+
+    def infomsg(msg):
+        global LOG
+        LOG.info('On executing %s:\n%s', sys.argv, msg)
+
+    def config_err(filename, msg):
+        err("violation in config file '{}': {}".format(filename, msg))
+
+    def valid_address(to):
+        if not isinstance(to, str):
+            return False
+        addresses = email.utils.getaddresses([to])
+        if len(addresses) == 1:
+            return addresses[0] != ('', '')
+        else:
+            return False
+
+    def get_address(to):
+        return email.utils.parseaddr(to)[1]
+
+    def get_address_without_delimiter(to, delim):
+        addr = get_address(to)
+        split = addr.rsplit('@', 1)
+        localpart = split[0]
+        if len(delim) == 0:
+            delim_pos = None
+        else:
+            positive = [ i for i in map(lambda d: localpart.find(d), delim) if i > 0 ]
+            if positive:
+                delim_pos = min(positive)
+            else:
+                delim_pos = None
+
+        new_localpart = localpart[:delim_pos]
+        if len(split) == 1:
+            return new_localpart
+        else:
+            return "{}@{}".format(new_localpart, split[1])
+
+    def load_config(filename, defaults):
+
+        config = defaults.copy()
+
+        with open(filename, encoding='utf-8') as f:
+            config.update(json.load(f))
+
+        # check for unknown config values
+
+        known_keys = [
+            'envelope_mail_from',
+            'envelope_rcpt_to',
+            'header_add',
+            'header_replace',
+            'require_from',
+            'require_sasl_username',
+            'sasl_recipient_delimiter',
+            'smtp_server',
+            'msg_bounced_requirements'
+        ]
+
+        for k in config:
+            if k not in known_keys:
+                config_err(filename, "unknown parameter '{}'".format(k))
+
+        # validate 'envelope_mail_from
+
+        if config['envelope_mail_from'] is not None:
+            if not valid_address(config['envelope_mail_from']):
+                config_err(
+                    filename,
+                    "'envelope_mail_from' must be a valid address")
+
+        # validate 'envelope_rcpt_to'
+
+        if config['envelope_rcpt_to'] is not None:
+            if not isinstance(config['envelope_rcpt_to'], list):
+                config_err(filename, "'envelope_rcpt_to' must be a list")
+
+            if not all([valid_address(s) for s in config['envelope_rcpt_to']]):
+                config_err(
+                    filename,
+                    "'envelope_rcpt_to' must be a list of valid addresses")
+
+        # validate 'header_add'
+
+        if not isinstance(config['header_add'], dict):
+            config_err(filename, "'header_add' must be a dict")
+
+        # validate 'header_replace'
+
+        if not isinstance(config['header_replace'], dict):
+            config_err(filename, "'header_replace' must be a dict")
+
+        # validate 'require_from'
+
+        if (config['require_from'] != False and
+            config['require_from'] != 'envelope_rcpt_to'):
+
+            if not isinstance(config['require_from'], list):
+                config_err(
+                    filename,
+                    "'require_from' must be a list, `false` or `\"envelope_rcpt_to\"`")
+
+            if not all([valid_address(s) for s in config['require_from']]):
+                config_err(
+                    filename,
+                    "'require_from' lists must only contain valid addresses")
+
+        # validate 'require_sasl_username'
+
+        if (config['require_sasl_username'] != False and
+            config['require_sasl_username'] != 'envelope_rcpt_to'):
+
+            if not isinstance(config['require_sasl_username'], list):
+                config_err(
+                    filename,
+                    "'require_sasl_username' must be a list, `false` or `\"envelope_rcpt_to\"`")
+
+            if not all([valid_address(s) for s in config['require_sasl_username']]):
+                config_err(
+                    filename,
+                    "'require_sasl_username' lists must only contain valid addresses")
+
+        if not isinstance(config['sasl_recipient_delimiter'], str):
+            config_err("'sasl_recipient_delimiter' must be a string or `false`")
+
+        return config
+
+    argparser = argparse.ArgumentParser(prog=PROG_NAME, description=PROG_NAME)
+    argparser.add_argument('--config', nargs='+', required=True)
+    argparser.add_argument('--sasl-username', default=None)
+
+    ARGS = argparser.parse_args()
+
+    config = collections.OrderedDict([
+        ('envelope_mail_from', None),
+        ('envelope_rcpt_to', None),
+        ('header_add', {}),
+        ('header_replace', {}),
+        ('require_from', False),
+        ('require_sasl_username', False),
+        ('sasl_recipient_delimiter', ""),
+        ('smtp_server', 'localhost:25'),
+        ('msg_bounced_requirements',
+         "You are not fulfilling all requirements for writing to this address.")
+    ])
+
+    # load configs
+
+    LOG.debug("Loading config files %s", ARGS.config)
+    for filename in ARGS.config:
+        config = load_config(filename, config)
+
+    LOG.debug("Using the following config:\n%s", config)
+
+    # check SASL username
+
+    if config['require_sasl_username'] != False:
+        LOG.debug("[require_sasl_username] Activated")
+
+        if not valid_address(ARGS.sasl_username):
+            err("Passed `--sasl-username {}` is not a valid username"
+                .format(ARGS.sasl_username))
+
+        if config['require_sasl_username'] == 'envelope_rcpt_to':
+            if config['envelope_rcpt_to'] is None:
+                config_err(
+                    ARGS.config[-1],
+                    "'envelope_rcpt_to' must be set if 'require_sasl_username' is used")
+            allowed_usernames = config['envelope_rcpt_to']
+        else:
+            allowed_usernames = config['require_sasl_username']
+
+        filterf = lambda x: get_address_without_delimiter(x, config['sasl_recipient_delimiter'])
+
+        if get_address(ARGS.sasl_username) not in map(filterf, allowed_usernames):
+            infomsg("[require_sasl_username] Rejected: Username is not in authorized list")
+            raise AuthenticationError(config['msg_bounced_requirements'])
+        
+        LOG.debug("[require_sasl_username] Passed")
+    else:
+        LOG.debug("[require_sasl_username] Not activated")
+
+    # parse email
+
+    LOG.debug("Parsing email")
+    msg = email.message_from_file(sys.stdin)
+
+    # check From Header
+
+    if config['require_from'] != False:
+        LOG.debug("[require_from] Activated")
+
+        if not valid_address(msg['From']):
+            infomsg("[require_from] Rejected: 'From' is not a valid address")
+            raise AuthenticationError(config['msg_bounced_requirements'])
+
+        if config['require_from'] == 'envelope_rcpt_to':
+            if config['envelope_rcpt_to'] is None:
+                config_err(
+                    ARGS.config[-1],
+                    "'envelope_rcpt_to' must be set if 'require_from' is used")
+            allowed_froms = config['envelope_rcpt_to']
+        else:
+            allowed_froms = config['require_from']
+
+        if get_address(msg['From']) not in map(get_address, allowed_froms):
+            infomsg("[require_from] Rejected: 'From' is not in authorized list")
+            raise AuthenticationError(config['msg_bounced_requirements'])
+
+        LOG.debug("[require_from] Passed")
+    else:
+        LOG.debug("[require_from] Not activated")
+
+    # update headers
+
+    for k in config['header_replace']:
+        if k in msg:
+            del msg[k]
+
+    for key, value in config['header_replace'].items():
+        msg[key] = value
+
+    for key, value in config['header_add'].items():
+        msg[key] = value
+
+    # send email
+
+    LOG.debug("Connecting to SMTP server at {}".format(config['smtp_server']))
+    smtp_conn = smtplib.SMTP(config['smtp_server'])
+
+    LOG.debug("Sending email via SMTP")
+    smtp_conn.send_message(
+        msg,
+        config['envelope_mail_from'],
+        config['envelope_rcpt_to'])
+
+except AuthenticationError as e:
+    LOG.debug("Rejecting message with 5.3.0")
+    print(
+        "5.3.0 {.args[0]}".format(e),
+        file=POSTFIX_PIPE_ERR)
+
+    sys.exit(1)
+
+except:
+    LOG.exception(
+        "Exception on executing %s:\n\nstdout:\n%s\nstderr:\n%s\n",
+        sys.argv, sys.stdout.getvalue(), sys.stderr.getvalue())
+
+    print(
+        "4.5.1 Requested action aborted - Local error in email list processing",
+        file=POSTFIX_PIPE_ERR)
+
+    sys.exit(1)
+
+LOG.debug("Shutting down normally")
+
